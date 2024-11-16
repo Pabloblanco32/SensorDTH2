ddcfa82edca2cf29a620ec8bb72264a04fa35ec1
pablo <pablo@DANIEL>
2024-11-16 09:54:26 -0600

    Agregar archivos de proyecto.

---
+++ SensorDTH2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SensorDTH2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelTemperature = new System.Windows.Forms.Label();
+            this.labelHumidity = new System.Windows.Forms.Label();
+            this.listBoxData = new System.Windows.Forms.ListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(15, 470);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPorts.TabIndex = 0;
+            this.comboBoxPorts.Text = "Combobox";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.BackColor = System.Drawing.Color.Lime;
+            this.buttonConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConnect.Location = new System.Drawing.Point(346, 297);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(143, 32);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "buttonconnect";
+            this.buttonConnect.UseVisualStyleBackColor = false;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.BackColor = System.Drawing.Color.Fuchsia;
+            this.buttonRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRefresh.Location = new System.Drawing.Point(300, 380);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(122, 28);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "buttonrefresh";
+            this.buttonRefresh.UseVisualStyleBackColor = false;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Red;
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(450, 380);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(92, 28);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "buttonexit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(12, 9);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(108, 24);
+            this.labelStatus.TabIndex = 4;
+            this.labelStatus.Text = "labelstatus";
+            // 
+            // labelTemperature
+            // 
+            this.labelTemperature.AutoSize = true;
+            this.labelTemperature.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTemperature.Location = new System.Drawing.Point(12, 211);
+            this.labelTemperature.Name = "labelTemperature";
+            this.labelTemperature.Size = new System.Drawing.Size(145, 20);
+            this.labelTemperature.TabIndex = 5;
+            this.labelTemperature.Text = "labeltemperature";
+            // 
+            // labelHumidity
+            // 
+            this.labelHumidity.AutoSize = true;
+            this.labelHumidity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHumidity.Location = new System.Drawing.Point(13, 417);
+            this.labelHumidity.Name = "labelHumidity";
+            this.labelHumidity.Size = new System.Drawing.Size(113, 20);
+            this.labelHumidity.TabIndex = 6;
+            this.labelHumidity.Text = "labelhumidity";
+            // 
+            // listBoxData
+            // 
+            this.listBoxData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.listBoxData.FormattingEnabled = true;
+            this.listBoxData.Location = new System.Drawing.Point(300, 40);
+            this.listBoxData.Name = "listBoxData";
+            this.listBoxData.Size = new System.Drawing.Size(242, 251);
+            this.listBoxData.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SensorDTH2.Properties.Resources.Captura_de_pantalla_2024_11_14_192706;
+            this.pictureBox2.Location = new System.Drawing.Point(15, 236);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(173, 167);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SensorDTH2.Properties.Resources.Captura_de_pantalla_2024_11_14_185248;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 159);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(578, 503);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBoxData);
+            this.Controls.Add(this.labelHumidity);
+            this.Controls.Add(this.labelTemperature);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Name = "Form1";
+            this.Text = "Temperatura_y_Humedad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelTemperature;
+        private System.Windows.Forms.Label labelHumidity;
+        private System.Windows.Forms.ListBox listBoxData;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ SensorDTH2/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SensorDTH2
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;
+        bool isConnected = false;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAvailablePorts();
+        }
+
+        private void LoadAvailablePorts()
+        {
+            comboBoxPorts.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            comboBoxPorts.Items.AddRange(ports);
+            if (ports.Length > 0)
+                comboBoxPorts.SelectedIndex = 0;
+            else
+                MessageBox.Show("No se encontraron puertos disponibles.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+                ConnectToSerialPort();
+            else
+                DisconnectFromSerialPort();
+        }
+
+        private void ConnectToSerialPort()
+        {
+            try
+            {
+                serialPort = new SerialPort(comboBoxPorts.SelectedItem.ToString(), 9600);
+                serialPort.DataReceived += SerialPort_DataReceived;
+                serialPort.Open();
+                isConnected = true;
+                buttonConnect.Text = "Desconectar";
+                labelStatus.Text = "Conectado";
+                labelStatus.ForeColor = System.Drawing.Color.Green;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DisconnectFromSerialPort()
+        {
+            try
+            {
+                if (serialPort != null && serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                    serialPort.Dispose();
+                }
+                isConnected = false;
+                buttonConnect.Text = "Conectar";
+                labelStatus.Text = "Desconectado";
+                labelStatus.ForeColor = System.Drawing.Color.Red;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al desconectar: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                // Leer datos del puerto serial
+                string data = serialPort.ReadLine();
+
+                // Asegurarse de procesarlo en el hilo de la interfaz gráfica
+                this.Invoke(new Action(() =>
+                {
+                    // Dividir los datos recibidos por el tabulador
+                    string[] values = data.Split('\t');
+
+                    // Validar que los datos estén completos (dos valores)
+                    if (values.Length == 2 &&
+                        float.TryParse(values[0], out float humedad) &&
+                        float.TryParse(values[1], out float temperatura))
+                    {
+                        // Actualizar etiquetas en la interfaz gráfica
+                        labelTemperature.Text = $"Temperatura: {temperatura} °C";
+                        labelHumidity.Text = $"Humedad: {humedad} %";
+
+                        // Agregar los datos al ListBox
+                        string logEntry = $"Temp: {temperatura} °C | Hum: {humedad} %";
+                        listBoxData.Items.Add(logEntry);
+
+                        // Opcional: Desplazarse automáticamente al final de la lista
+                        listBoxData.TopIndex = listBoxData.Items.Count - 1;
+                    }
+                    else
+                    {
+                        // Mostrar un mensaje en caso de error en los datos
+                        listBoxData.Items.Add("Datos inválidos recibidos.");
+                    }
+                }));
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores durante la recepción de datos
+                this.Invoke(new Action(() =>
+                {
+                    listBoxData.Items.Add($"Error: {ex.Message}");
+                }));
+            }
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            LoadAvailablePorts();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DisconnectFromSerialPort();
+            Application.Exit();
+        }
+    }
+}
